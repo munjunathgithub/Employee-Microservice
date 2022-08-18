149a3ad2c7bc654631cb81c401530ebea7c00075
unknown <cogjava3100@iiht.tech>
2022-08-18 13:12:39 +0530

    added records

--- src/main/java/com/emp/Service/EmployeeServiceImpl.java
+++ src/main/java/com/emp/Service/EmployeeServiceImpl.java
@@ -13,14 +13,14 @@ public class EmployeeServiceImpl implements IEmployeeService {
 	
 	List<Employee> list = List.of(
 			new Employee(1311L,"Peter Parker","65478654"),
-			new Employee(1312L,"Peter Parker","65478654"),
-			new Employee(1313L,"Peter Parker","65478654"),
-			new Employee(1314L,"Peter Parker","65478654"),
-			new Employee(1315L,"Peter Parker","65478654"),
-			new Employee(1316L,"Peter Parker","65478654"),
-			new Employee(1317L,"Peter Parker","65478654"),
-			new Employee(1318L,"Peter Parker","65478654"),
-			new Employee(1319L,"Peter Parker","65478654")
+			new Employee(1312L,"Peter Par","79664879876"),
+			new Employee(1313L,"Peter k","8659766676"),
+			new Employee(1314L,"dab dell","374854825"),
+			new Employee(1315L,"man tenan","3485839384"),
+			new Employee(1316L,"jan jhon","83748564838"),
+			new Employee(1317L,"San Parker","837455840"),
+			new Employee(1318L,"tom Parker","937475854"),
+			new Employee(1319L,"bob Parker","93845857484")
 			
 			);
 
