dda66358e10a06f9e89a909d5492e635d8d3d1f6
unknown <cogjava3100@iiht.tech>
2022-08-18 10:45:07 +0530

    added services

--- src/main/java/com/emp/Service/EmployeeServiceImpl.java
+++ src/main/java/com/emp/Service/EmployeeServiceImpl.java
@@ -1,5 +1,33 @@
 package com.emp.Service;
 
-public class EmployeeServiceImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.emp.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements IEmployeeService {
+	
+	//Dummy Employee list 
+	
+	List<Employee> list = List.of(
+			new Employee(1311L,"Peter Parker","65478654"),
+			new Employee(1312L,"Peter Parker","65478654"),
+			new Employee(1313L,"Peter Parker","65478654"),
+			new Employee(1314L,"Peter Parker","65478654"),
+			new Employee(1315L,"Peter Parker","65478654"),
+			new Employee(1316L,"Peter Parker","65478654"),
+			new Employee(1317L,"Peter Parker","65478654"),
+			new Employee(1318L,"Peter Parker","65478654"),
+			new Employee(1319L,"Peter Parker","65478654")
+			
+			);
+
+	@Override
+	public Employee getEmployee(Long id) {
+		// TODO Auto-generated method stub
+		return list.stream().filter(employee -> employee.geteId().equals(id)).findAny().orElse(null);
+	}
 
 }
--- src/main/java/com/emp/Service/IEmployeeService.java
+++ src/main/java/com/emp/Service/IEmployeeService.java
@@ -1,5 +1,9 @@
 package com.emp.Service;
 
+import com.emp.entity.Employee;
+
 public interface IEmployeeService {
+	
+	public Employee getEmployee(Long id);
 
 }
